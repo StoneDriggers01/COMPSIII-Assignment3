79286d3ac42450dd55b42940aa34d9071b98c2f1
StoneDriggers01 <stonedriggersofficial@gmail.com>
2025-09-21 19:50:16 -0400

    Parent file created

--- school.py
+++ school.py
@@ -1,6 +1,12 @@
 class Person:
-    # Delete pass and add your code here
-    pass
+    def __init__(self, name: str, age: int, country: str):
+        self.name = name
+        self.age = age
+        self.country = country
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old and is from {self.country}."
+        #Should Return NAME is AGE and is from COUNTRY
 
 class Student(Person):
     # Delete pass and add your code here
