e5d37608929ea20f9009862108a497eb39109934
StoneDriggers01 <stonedriggersofficial@gmail.com>
2025-09-21 20:09:03 -0400

    Staff and Student classes created

--- school.py
+++ school.py
@@ -6,12 +6,22 @@ class Person:
 
     def __str__(self):
         return f"{self.name} is {self.age} years old and is from {self.country}."
-        #Should Return NAME is AGE and is from COUNTRY
+        #Should Return: NAME is AGE and is from COUNTRY.
 
 class Student(Person):
-    # Delete pass and add your code here
-    pass
+    def __init__(self, name: str, age: int, country: str, major: str, gpa: float):
+        super().__init__(name, age, country)
+        self.major = major
+        self.gpa = gpa
+    def study(self):
+        return f"{self.name} is studying {self.major} with a current GPA of {self.gpa}."
 
 class Staff(Person):
-    # Delete pass and add your code here
-    pass
+    def __init__(self, name: str, age: int, country: str, position: str, department: str):
+        super().__init__(name, age, country)
+        self.position = position
+        self.department = department
+
+    def __str__(self):
+        return f"{self.name} works as a {self.position} in the {self.department} department."
+    #Should Return: NAME works as a POSITION in the DEPARTMENT department. 
